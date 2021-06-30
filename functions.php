ab8f05a1d8b1870be2f77eb5885e044ea9d4ddee
Code IT Hub <78258092+codeithub@users.noreply.github.com>
2021-06-30 10:26:56 +0530

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,50 @@
+function woocommerce_quantity_input( $args = array(), $product = null, $echo = true ) {
+  
+   if ( is_null( $product ) ) {
+      $product = $GLOBALS['product'];
+   }
+ 
+   $defaults = array(
+      'input_id' => uniqid( 'quantity_' ),
+      'input_name' => 'quantity',
+      'input_value' => '1',
+      'classes' => apply_filters( 'woocommerce_quantity_input_classes', array( 'input-text', 'qty', 'text' ), $product ),
+      'max_value' => apply_filters( 'woocommerce_quantity_input_max', -1, $product ),
+      'min_value' => apply_filters( 'woocommerce_quantity_input_min', 0, $product ),
+      'step' => apply_filters( 'woocommerce_quantity_input_step', 1, $product ),
+      'pattern' => apply_filters( 'woocommerce_quantity_input_pattern', has_filter( 'woocommerce_stock_amount', 'intval' ) ? '[0-9]*' : '' ),
+      'inputmode' => apply_filters( 'woocommerce_quantity_input_inputmode', has_filter( 'woocommerce_stock_amount', 'intval' ) ? 'numeric' : '' ),
+      'product_name' => $product ? $product->get_title() : '',
+   );
+ 
+   $args = apply_filters( 'woocommerce_quantity_input_args', wp_parse_args( $args, $defaults ), $product );
+  
+   
+   $args['min_value'] = max( $args['min_value'], 0 );
+   $args['max_value'] = 0 < $args['max_value'] ? $args['max_value'] : 20;
+
+   if ( '' !== $args['max_value'] && $args['max_value'] < $args['min_value'] ) {
+      $args['max_value'] = $args['min_value'];
+   }
+  
+   $options = '';
+    
+   for ( $count = $args['min_value']; $count <= $args['max_value']; $count = $count + $args['step'] ) {
+
+      if ( '' !== $args['input_value'] && $args['input_value'] >= 1 && $count == $args['input_value'] ) {
+        $selected = 'selected';      
+      } else $selected = '';
+ 
+      $options .= '<option value="' . $count . '"' . $selected . '>' . $count . '</option>';
+ 
+   }
+     
+   $string = '<div class="quantity"><span>Qty</span><select name="' . $args['input_name'] . '">' . $options . '</select></div>';
+ 
+   if ( $echo ) {
+      echo $string;
+   } else {
+      return $string;
+   }
+  
+}
